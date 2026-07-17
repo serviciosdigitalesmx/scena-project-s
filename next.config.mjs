f1bc6edbbe1031943fb69e22367ea4589979af6b
usuario <jesusvilla091921@gmail.com>
2026-07-17 10:20:53 -0600

    fix: redirect apex domain to www

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'scena.com.mx',
+          },
+        ],
+        destination: 'https://www.scena.com.mx/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
